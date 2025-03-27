5c28c0ec653a1d7f9bcb51604ad8006acb962f31
luishaedo <luismariahaedolanda@gmail.com>
2025-03-27 18:14:18 -0300

    Corrige login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -85,7 +85,7 @@ exports.login = async (req, res) => {
     console.log("contraseña password",password);
     console.log("contraseña user.password",user.password);
     // Verificar si la contraseña en la BD está encriptada
-    if (!user.password.startsWith("$2a$")) {
+    if (user.password.startsWith("$2a$")) {
       console.log("Error: La contraseña no está encriptada correctamente en la base de datos.");
       return res.status(500).json({ message: "Error del servidor" });
     }
